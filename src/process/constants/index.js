e4a2dc847d256d5587eab2042c2d954b6a130e90
seadonggyun4 <seadonggyun@gmail.com>
2024-12-04 14:07:58 +0900

    [fix]: create constants

---
+++ src/process/constants/index.js
@@ -0,0 +1,6 @@
+export const URL_QUERY = 'https://api.binance.com/api/v3'
+export const URL_SOCKET = 'wss://stream.binance.com:9443'
+
+// react Query
+export const STALE_TIME = 5000
+export const CACHE_TIME = 60000
