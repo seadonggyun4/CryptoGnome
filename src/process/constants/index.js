107aab554432c04bce5755fb13f7f72f349c7942
seadonggyun4 <seadonggyun@gmail.com>
2024-12-04 15:14:17 +0900

    [fix]: Change the react-query cache time

--- src/process/constants/index.js
+++ src/process/constants/index.js
@@ -2,5 +2,5 @@ export const URL_QUERY = 'https://api.binance.com/api/v3'
 export const URL_SOCKET = 'wss://stream.binance.com:9443'
 
 // react Query
-export const STALE_TIME = 5000
-export const CACHE_TIME = 60000
+export const STALE_TIME = 300
+export const CACHE_TIME = 1000
