bfd8c0315ed7e98933f7de0998d50a7078e7e45f
seadonggyun4 <seadonggyun@gmail.com>
2024-12-12 14:30:32 +0900

    [fix]: disable google login logic

--- src/app/en/trade/BTCUSDT/page.tsx
+++ src/app/en/trade/BTCUSDT/page.tsx
@@ -3,7 +3,7 @@ import { WebSocketProvider } from "@/process/webSocket/provider/WebSocketContext
 import { TradingContextProvider } from "@/app/en/trade/BTCUSDT/provider/TradingContext";
 import { ToastProvider } from "@/app/common/provider/ToastContext";
 import { MetaMaskAuthProvider } from "@/auth/metamask/provider/MetaMaskAuthProvider";
-import { GoogleAuthProvider } from "@/auth/google/provider/GoogleAuthProvider";
+// import { GoogleAuthProvider } from "@/auth/google/provider/GoogleAuthProvider";
 import HomeSection from "@/app/en/trade/BTCUSDT/HomeSection";
 import Header from "@/app/common/components/Header";
 import VideoBg from "@/app/common/components/VideoBg";
@@ -12,7 +12,7 @@ export default function BTCUSDTPage() {
   return (
     <ReactQueryProvider>
         <ToastProvider>
-            <GoogleAuthProvider>
+            {/*<GoogleAuthProvider>*/}
                 <MetaMaskAuthProvider>
                     <TradingContextProvider>
                         <WebSocketProvider>
@@ -22,7 +22,7 @@ export default function BTCUSDTPage() {
                         </WebSocketProvider>
                     </TradingContextProvider>
                 </MetaMaskAuthProvider>
-            </GoogleAuthProvider>
+            {/*</GoogleAuthProvider>*/}
         </ToastProvider>
     </ReactQueryProvider>
   );
